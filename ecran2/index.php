30799d8b1f34c589d8d4f7c56fcafa060afcfe51
ClamSade <89941944+ClamSade@users.noreply.github.com>
2022-05-19 15:12:49 +0200

    bouton nouvelle filiere

--- ecran2/index.php
+++ ecran2/index.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Tableau de bord</title>
-</head>
-<body>
-        <nav>
-            <ul>
-                <li id="menu"><a href="#">MENU</a>
-                    <ul>
-                        <li><a href="#">ecran1</a></li>
-                        <li><a href="#">ecran2</a></li>
-                        <li><a href="#">ecran3</a></li>
-                    </ul>
-                </li>
-                <li id="tdb">TABLEAU DE BORD</li>
-                <li id="deco"><a href="#">DECONNEXION</a></li>
-            </ul>
-        </nav>
-        <a href="../ecran4/index.php"></a>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Tableau de bord</title>
+    </head>
+    <body>
+        <div class="nav">
+            <nav>
+                <ul>
+                    <li id="menu"><a href="#">MENU</a>
+                        <ul>
+                            <li><a href="#">ecran1</a></li>
+                            <li><a href="#">ecran2</a></li>
+                            <li><a href="#">ecran3</a></li>
+                        </ul>
+                    </li>
+                    <li id="tdb">TABLEAU DE BORD</li>
+                    <li id="deco"><a href="#">DECONNEXION</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="ajout_filiere">
+            <a href="../ecran4/index.php">+ AJOUTER UNE NOUVELLE FILIERE</a>
+        </div>
+        
+    </body>
 </html>
