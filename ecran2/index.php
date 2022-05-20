f30f3a8a5b2c8e635651594d4e53ef83dea6cce3
ClamSade <89941944+ClamSade@users.noreply.github.com>
2022-05-20 11:07:36 +0200

    créaton de table pour la nav

--- ecran2/index.php
+++ ecran2/index.php
@@ -3,31 +3,30 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styles.css">
         <link rel="stylesheet" href="../normalize.css">
         <title>Tableau de bord</title>
     </head>
     <body>
-        
-            <nav>
-                <ul>
-                    <li><a href="#">MENU</a>
+        <table>
+            <tr>
+                <td class="menu">
+                    <nav><ul><li><a href="#">MENU</a>
                         <ul>
-                            <li><a href="../ecran1/index.php">Connexion</a></li>
+                        <li><a href="../ecran1/index.php">Connexion</a></li>
                             <li><a href="../ecran2/index.php">Tableau de bord</a></li>
                             <li><a href="../ecran3/index.php">Calendrier</a></li>
                             <li><a href="../ecran4/index.php">Nouvelle filière</a></li>
                             <li><a href="../ecran5/index.php">Nouvelle session</a></li>
                         </ul>
-                    </li>
-                    <li>TABLEAU DE BORD</li>
-                    <li><a href="#">DECONNEXION</a></li>
-                </ul>
-            </nav>
-        
+                    </li></ul></nav>  
+                </td>
+                <td class="tdb">TABLEAU DE BORD</td>
+                <td class="deco"><button>DECONNEXION</button></td>
+            </tr>
+        </table>
         <div class="ajout_filiere">
             <a href="../ecran4/index.php">+ AJOUTER UNE NOUVELLE FILIERE</a>
         </div>
-        
-</body>
+    </body>
 </html>
