044daf4ed7adb50e4d67ef905a4ccdd7e5316d0d
ClamSade <89941944+ClamSade@users.noreply.github.com>
2022-05-30 11:08:55 +0200

    ecran 4

---
+++ ecran4.php/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="../normalize.css">
+        <title>Tableau de bord</title>
+    </head>
+    <body>
+        <table class="nav">
+            <tr>
+                <td class="menu">
+                    <nav><ul><li><a href="#"><img src="menu.png"/></a>
+                        <ul>
+                        <li><a href="../ecran1/index.php">Connexion</a></li>
+                            <li><a href="../ecran2/index.php">Tableau de bord</a></li>
+                            <li><a href="../ecran3/index.php">Calendrier</a></li>
+                            <li><a href="../ecran4/index.php">Nouvelle filière</a></li>
+                            <li><a href="../ecran5/index.php">Nouvelle session</a></li>
+                        </ul>
+                    </li></ul></nav>  
+                </td>
+                <td class="tdb">TABLEAU DE BORD</td>
+                <td class="deco"><button><img src="déconnexion.png"></button></td>
+            </tr>
+        </table>
+       
+        <section>
+            <table class="filiere">
+               
+            </table>
+        </section>
+    </body>
+</html>
